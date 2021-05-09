efe82764596f0b5f54c0d1773c959c28a467fae8
boua07 <ahmed.bouachir.05@gmail.com>
2021-05-09 01:45:14 +0100

    heroku

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(
 );
 
 app.use(express.static("public"));
-app.use(cors({ origin: "http://fac-academy.ureachus.com/" }));
+app.use(cors({ origin: "http://fac-academy.ureachus.com" }));
 
 // connecting to mongodb
 
